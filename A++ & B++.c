36eabeba03d560f6282b317715043a98a952f911
KanigaSri <kanigasri31@gmail.com>
2026-02-16 18:40:42 +0530

    Create A++ & B++.c

---
+++ A++ & B++.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 2, b = 3;
+
+    printf("Initial values: a = %d, b = %d\n", a, b);
+
+    printf("Using a++ : %d\n", a++);
+    printf("Using b++ : %d\n", b++);
+
+    printf("After increment: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
