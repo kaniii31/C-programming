2078d444d4d410fc28fe24a5c594a4a5950db1d9
KanigaSri <kanigasri31@gmail.com>
2026-02-19 20:43:59 +0530

    Create transpose of a matrix.c

---
+++ transpose of a matrix.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int r, c, i, j;
+    int a[10][10], trans[10][10];
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter matrix elements:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            trans[j][i] = a[i][j];
+        }
+    }
+
+    printf("Transpose matrix:\n");
+    for(i = 0; i < c; i++) {
+        for(j = 0; j < r; j++) {
+            printf("%d ", trans[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
